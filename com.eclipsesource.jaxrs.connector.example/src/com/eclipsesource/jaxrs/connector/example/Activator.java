4474d3bc684fa18ffc971476bd5cf4c07862f1de
hstaudacher <hstaudacher@eclipsesource.com>
2012-01-19 10:49:15 +0100

    Updated target

--- com.eclipsesource.jaxrs.connector.example/src/com/eclipsesource/jaxrs/connector/example/Activator.java
+++ com.eclipsesource.jaxrs.connector.example/src/com/eclipsesource/jaxrs/connector/example/Activator.java
@@ -10,11 +10,8 @@
  ******************************************************************************/
 package com.eclipsesource.jaxrs.connector.example;
 
-import java.util.Hashtable;
-
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.Constants;
 import org.osgi.framework.ServiceRegistration;
 
 
@@ -24,10 +21,8 @@ public class Activator implements BundleActivator {
 
   @Override
   public void start( BundleContext context ) throws Exception {
-    Hashtable<String, String> properties = new Hashtable<String, String>();
-    properties.put( Constants.SERVICE_PID, ExampleService.class.getName() );
     ExampleService exampleService = new ExampleService();
-    registration = context.registerService( ExampleService.class.getName(), exampleService, properties );
+    registration = context.registerService( ExampleService.class.getName(), exampleService, null );
   }
 
   @Override
