6ea4a4079e69cfdca08a12cd2e278eab75fb506e
hstaudacher <hstaudacher@eclipsesource.com>
2012-11-26 18:19:09 +0100

    Added API Documentation. Added copyright headers.

--- com.eclipsesource.jaxrs.consumer/src/com/eclipsesource/jaxrs/consumer/ConsumerFactory.java
+++ com.eclipsesource.jaxrs.consumer/src/com/eclipsesource/jaxrs/consumer/ConsumerFactory.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2012 EclipseSource and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Holger Staudacher - initial API and implementation
+ ******************************************************************************/
 package com.eclipsesource.jaxrs.consumer;
 
 import java.lang.reflect.Proxy;
@@ -5,16 +15,47 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 import javax.ws.rs.Path;
+import javax.ws.rs.ext.Provider;
 
 import com.eclipsesource.jaxrs.consumer.internal.ResourceInvocationHandler;
 
 
+/**
+ * <p>
+ * A static factory for creating consumer objects out of <code>@Path</code> annotated interfaces.
+ * </p>
+ * 
+ * @see Path
+ * @see Provider
+ */
 public class ConsumerFactory {
 
+  /**
+   * <p>
+   * Creates a consumer object out of a <code>@Path</code> interface. It will create a proxy object of the interface
+   * that calls the specified service using the passed in base url.
+   * </p>
+   *   
+   * @param baseUrl The server url hosting the specified service.
+   * @param type The <code>@Path</code> annotated interface class object.
+   * @return a proxy object for the passed in type.
+   */
   public static <T> T createConsumer( String baseUrl, Class<T> type ) {
     return createConsumer( baseUrl, type, new Object[] {} );
   }
   
+  /**
+   * <p>
+   * Creates a consumer object out of a <code>@Path</code> interface. It will create a proxy object of the interface
+   * that calls the specified service using the passed in base url. The de/serialization is done using the passed in
+   * <code>@Provider</code> objects.
+   * </p>
+   *   
+   * @param baseUrl The server url hosting the specified service.
+   * @param type The <code>@Path</code> annotated interface class object.
+   * @param customProvider An array of <code>@Provider</code> object for de/serialization.
+   * @return a proxy object for the passed in type.
+   */
   @SuppressWarnings( "unchecked" )
   public static <T> T createConsumer( String baseUrl, Class<T> type, Object... customProvider ) {
     validateArguments( baseUrl, type );
--- examples/com.eclipsesource.jaxrs.consumer.example/src/com/eclipsesource/jaxrs/consumer/example/Publisher.java
+++ examples/com.eclipsesource.jaxrs.consumer.example/src/com/eclipsesource/jaxrs/consumer/example/Publisher.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2012 EclipseSource and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Holger Staudacher - initial API and implementation
+ ******************************************************************************/
 package com.eclipsesource.jaxrs.consumer.example;
 
 import com.eclipsesource.jaxrs.consumer.ConsumerPublisher;
