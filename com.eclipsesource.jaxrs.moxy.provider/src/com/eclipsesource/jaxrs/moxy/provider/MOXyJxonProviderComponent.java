8c609bd915fc4dec914435781e87fee9da5b02d6
Bryan Hunt <bhunt@mac.com>
2013-09-26 21:17:59 -0500

    Added MOXy support

---
+++ com.eclipsesource.jaxrs.moxy.provider/src/com/eclipsesource/jaxrs/moxy/provider/MOXyJxonProviderComponent.java
@@ -0,0 +1,19 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Bryan Hunt.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Bryan Hunt - initial API and implementation
+*******************************************************************************/
+package com.eclipsesource.jaxrs.moxy.provider;
+
+import javax.ws.rs.ext.Provider;
+
+import org.eclipse.persistence.jaxb.rs.MOXyJsonProvider;
+
+@Provider
+public class MOXyJxonProviderComponent extends MOXyJsonProvider 
+{}
