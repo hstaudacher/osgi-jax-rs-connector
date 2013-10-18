cf1459bc94dc1e7182385b667909400b79a1961a
Bryan Hunt <bhunt@mac.com>
2013-10-17 21:46:03 -0500

    Remove dependency on DS

--- bundles/com.eclipsesource.jaxrs.provider.security/src/com/eclipsesource/jaxrs/provider/security/SecurityContextProviderComponent.java
+++ bundles/com.eclipsesource.jaxrs.provider.security/src/com/eclipsesource/jaxrs/provider/security/SecurityContextProviderComponent.java
@@ -16,32 +16,27 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.core.SecurityContext;
 
+import com.eclipsesource.jaxrs.provider.security.bundle.Activator;
 import com.eclipsesource.jaxrs.security.AuthenticationService;
 import com.eclipsesource.jaxrs.security.AuthorizationService;
 import com.eclipsesource.jaxrs.security.SecurityContextProvider;
 
 public class SecurityContextProviderComponent implements SecurityContextProvider {
-    private volatile AuthenticationService authenticationService;
-    private volatile AuthorizationService authorizationService;
 
     @Override
     public SecurityContext getSecurityContext(ContainerRequestContext requestContext)
     {
-    	Principal principal = authenticationService.authenticate(requestContext);
+        AuthenticationService authenticationService = Activator.getInstance().getAuthenticationService();
+        AuthorizationService authorizationService = Activator.getInstance().getAuthorizationService();
+
+        if(authenticationService == null || authorizationService == null)
+        	return null;
+        
+        Principal principal = authenticationService.authenticate(requestContext);
 
     	if (principal == null)
     		requestContext.abortWith(Response.status(Status.UNAUTHORIZED).build());
 
     	return new AuthSecurityContext(authenticationService.getAuthenticationScheme(), principal, requestContext.getUriInfo().getRequestUri().getScheme().equals("https"), authorizationService);
-    }
-
-    public void bindAuthenticationService(AuthenticationService authenticationService)
-    {
-    	this.authenticationService = authenticationService;
-    }
-
-    public void bindAuthorizationService(AuthorizationService authorizationService)
-    {
-    	this.authorizationService = authorizationService;
     }
 }
